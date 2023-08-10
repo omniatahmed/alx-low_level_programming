389dfc0bf9418c6913935a34af69ecdf7981a2dd
omniatahmed <omniatalnour2022@gmail.com>
2023-08-10 16:37:32 -0700

    r

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {      
-	printf("size of a char: %d byte(s)\n,. sizeof(char));
+	printf("size of a char: %d byte(s)\n,  sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
-	rintf("size of along int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long));
 	printf("size of a long long int: %d byte(s)\n", sizeof((double));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
